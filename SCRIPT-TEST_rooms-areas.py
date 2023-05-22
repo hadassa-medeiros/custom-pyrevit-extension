ed83a31c09c8d42bfbbe1ac7c02f85d88d2be7f6
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-05-22 17:04:37 -0300

    Last alterations explained

--- SCRIPT-TEST_rooms-areas.py
+++ SCRIPT-TEST_rooms-areas.py
@@ -6,6 +6,8 @@ doc = __revit__.ActiveUIDocument.Document
 rooms = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms)
 
 for r in rooms:
+	#antes, ao não fazer a conversão, o valor estava em pés, que descobri ser o default
+	# do revit. logo, a conversão necessária para m²:
 	area_amb = round(((r.Area*0.3048)/3.2808),2)
 	nome = Element.Name.GetValue(r)
 	print('{}-{}m'.format(nome, area_amb))
