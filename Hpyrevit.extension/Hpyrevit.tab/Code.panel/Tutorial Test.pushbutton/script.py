04d53b4549d403a9db3146aae72afc415415bf0f
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-05-26 12:50:06 -0300

    Testing how synthax works inside Revit

--- Hpyrevit.extension/Hpyrevit.tab/Code.panel/Tutorial Test.pushbutton/script.py
+++ Hpyrevit.extension/Hpyrevit.tab/Code.panel/Tutorial Test.pushbutton/script.py
@@ -21,13 +21,20 @@ print(e_id)
 print(element)
 
 #trying to acces the parameter room_name of some random room:
-room_id = revit.ElementId(1123259)  # Replace 123 with the actual ElementId
+room1_id = revit.ElementId(1123259)  # Replace 123 with the actual ElementId
+room2_id = revit.ElementId(347079)
 # Get the room element from the ElementId
-room = doc.GetElement(room_id)
+room1 = doc.GetElement(room1_id)
+room2 = doc.GetElement(room2_id)
+param_set1 = room1.Parameters.Size
 
-parameter_set = room.Parameters
-print(parameter_set)
 
+#now, testing how to get information from multiple elements inside the project(proper loops and methods to be used
+# as nexts steps):
+list_of_rooms = [room1, room2]
+for room in list_of_rooms:
+    param_set = room.Parameters.Size
+    print('{} has {} parameters inside it.'.format(room,param_set))
 
 
 # room_name_param = parameter_set[revit.BuiltInParameter.ROOM_NAME]
