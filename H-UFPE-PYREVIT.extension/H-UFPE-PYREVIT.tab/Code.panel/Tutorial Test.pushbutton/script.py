1fbb4f2b0d7b3a308fa0d55db2559e00cb81743c
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-06-29 11:41:51 -0300

    Testing how synthax works inside Revit

--- H-UFPE-PYREVIT.extension/H-UFPE-PYREVIT.tab/Code.panel/Tutorial Test.pushbutton/script.py
+++
@@ -1,6 +0,0 @@
-from pyrevit import forms, revit, DB, UI
-import Autodesk.Revit.DB as revit
-
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
-app = __revit__.Application
