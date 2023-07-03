1c0c4b03c3b294be3663c1945fe504ccbd02a68b
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-07-03 11:20:05 -0300

    Testing how synthax works inside Revit

--- H-UFPE-PYREVIT.extension/H-UFPE-PYREVIT.tab/Code.panel/APRENDENDO A USAR A API.pushbutton/script.py
+++ H-UFPE-PYREVIT.extension/H-UFPE-PYREVIT.tab/Code.panel/APRENDENDO A USAR A API.pushbutton/script.py
@@ -6,18 +6,57 @@ uidoc = __revit__.ActiveUIDocument
 app = __revit__.Application
 
 # tutorial parameteres:https://www.youtube.com/watch?v=YoLxucuC6Ss
-wall_id = doc.GetElement(revit.ElementId(335641))
+#ATÉ O FIM, DEMONSTRAÇAO DE QUE É POSSIVEL ACESSAR INFORMAÇAO OU POR UM ID DE OBJETO
+#JA CONHECIDO (wall_id) ou iterando por uma lista de objetos (all_walls)
+all_walls = revit.FilteredElementCollector(doc).OfCategory(revit.BuiltInCategory.OST_Walls)
+
+# for wall in all_walls:
+#     print(wall.Id)
+    # for p in wall.Parameters:
+    #     # print(p.Definition.Name)
+    #     wall_c = wall.get_Parameter(revit.BuiltInParameter.ALL_MODEL_TYPE_NAME)
+    #     print(wall_c.AsString())
+    # print(wall.Name) #nao funciona!! nao faz sentido pq Name é um mebro (DO TIPO eLEMENT) DA wALLtYPE
+
+materiais = revit.FilteredElementCollector(doc).OfCategory(revit.BuiltInCategory.OST_Materials)
+# for material in materiais:
+#     print(material.Name)
+#     print('O material se chama {} e está na categoria {}.'.format(material.Name, material.Category.Name))
+    # print(material.IsValidObject)
+
+wall_id = doc.GetElement(revit.ElementId(343830))
 # print(list(wall_id.Parameters))
-
 # for p in wall_id.Parameters:
-#     print('\n{}'.format(p.Definition.Name))
-#     print(p.Definition.BuiltInParameter)
-#     print(p.IsReadOnly)
-#     print(p.StorageType)
+    #     print('\n{}'.format(p.Definition.Name))
+    #     print(p.Definition.BuiltInParameter)
+    #     print(p.IsReadOnly)
+    #     print(p.StorageType)
 
-print(wall_id.GetMaterialIds.Id)
+wall_c = wall_id.get_Parameter(revit.BuiltInParameter.ELEM_TYPE_PARAM)
+print(wall_c.AsValueString()) #asvaluestring funciona, asstring nao funciona.
+
+ambiente_hall = doc.GetElement(revit.ElementId(611306))
+# print(ambiente_hall.LookUpParameter('COD-REVEST_PAREDES'))
+
+#abaixo, ele encontrou o parametro compartilhado de revest de paredes no ambiente especificado.
+for a in ambiente_hall.Parameters:
+    nome_param = a.Definition.Name
+    if nome_param == 'COD-REVEST_PAREDES':
+        print(nome_param)
 
 
+
+# rev_parede_hall = ambiente_hall.get_Parameter(revit.)
+
+
+
+#print(wall_id.GetMaterialVolume(??))
+# print(mats)
+# for m in mats:
+#     print(m.Definition.Name)
+
+#FIM
+#---------------------------------------------------------------------------
 
 
 # area_tags = revit.FilteredElementCollector(doc).OfCategory(revit.BuiltInCategory.OST_AreaTags).ToElements()
