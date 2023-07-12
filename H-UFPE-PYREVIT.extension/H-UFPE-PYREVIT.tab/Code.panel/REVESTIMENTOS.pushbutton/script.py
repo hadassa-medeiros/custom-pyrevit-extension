0d386ea3b89374601edbfd3a16e1bcd54b9b0317
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2023-07-12 11:56:10 -0300

    Funcionando, erro do forro corrigido (agora, identificando o objeto de forro correto, referente ao ambiente)

--- H-UFPE-PYREVIT.extension/H-UFPE-PYREVIT.tab/Code.panel/REVESTIMENTOS.pushbutton/script.py
+++ H-UFPE-PYREVIT.extension/H-UFPE-PYREVIT.tab/Code.panel/REVESTIMENTOS.pushbutton/script.py
@@ -27,73 +27,58 @@ for id in ids:
     ambientes_em_teste.append(amb)
 print('Localizadas XYZ bounding box dos seguintes elementos:')
 
-for ambiente in ambientes_em_teste:
-    ambiente_level = ambiente.Level.Name
-    ambiente_id = ambiente.Id.ToString()
-    ambiente_name = ambiente.get_Parameter(DB.BuiltInParameter.ROOM_NAME).AsString()
-    ambiente_elem = doc.GetElement(DB.ElementId(int(ambiente_id)))
-    ambiente_bbox = ambiente.get_BoundingBox(doc.ActiveView)
-#_______________________________________________________________________
-    # #transofrmar o type XYZ tanto do min como do max da boundingbox do ambiente,
-    # # pra conseguir aplicar fator, pra deixar os limites do ambiente "menores",
-    # # na intenção de que o filtro de elem intersectantes não "colete" elem construtivos
-    # # de ambientex adjacentes àquele do qual se desej aobter os revestimentos.
-    # #porx ex: ele nao retornaria mais o forro do ambiente hall no mezanino, apenas por ele
-    # # estar bem próximo ao limite do ambiente Copa.
-    # min0 = float(ambiente_bbox.Min[0]) #10.726396151
-    # min1 = float(ambiente_bbox.Min[1]) # -89.358506603
-    # min2 = float(ambiente_bbox.Min[2]) #11.975065617
-    # max0 = float((ambiente_bbox.Max[0])) # 26.441619248
-    # max1 = float((ambiente_bbox.Max[1]) * 1.3) # -72.964331803
-    # max2 = float((ambiente_bbox.Max[2]) * 1.1)  # 18.536745407
+for amb in ambientes_em_teste:
+    amb_level = amb.Level.Name
+    amb_id = amb.Id.ToString()
+    amb_name = amb.get_Parameter(DB.BuiltInParameter.ROOM_NAME).AsString()
+    amb_elem = doc.GetElement(DB.ElementId(int(amb_id)))
+    amb_area_str = amb.LookupParameter('Área').AsValueString()
+    amb_area = float((amb_area_str)[:5])
+    fator_multipl_m2_para_double = (amb.LookupParameter('Área').AsDouble()) / amb_area_num
+    amb_bbox = amb.get_BoundingBox(doc.ActiveView)
+    amb_outline = DB.Outline(amb_bbox.Min,amb_bbox.Max)  # é a outline que se passa como arg pro método DB.BoundingBoxIntersectsFilter (e não um objeto tipo BoundingBoxXYZ
 
-    # pos_conversao_ambiente_bbox_MIN = DB.XYZ(min0,min1,min2)
-    # pos_conversao_ambiente_bbox_MAX = DB.XYZ(max0, max1, max2)
-    ambiente_outline = DB.Outline(ambiente_bbox.Min,ambiente_bbox.Max)  # é a outline que se passa como arg pro método DB.BoundingBoxIntersectsFilter (e não um objeto tipo BoundingBoxXYZ
-    # ambiente_outline = DB.Outline(pos_conversao_ambiente_bbox_MIN, ambiente_bbox.Max) #é a outline que se passa como arg pro método DB.BoundingBoxIntersectsFilter (e não um objeto tipo BoundingBoxXYZ
-    # ambiente_outline = DB.Outline(ambiente_bbox.Min, pos_conversao_ambiente_bbox_MAX) #é a outline que se passa como arg pro método DB.BoundingBoxIntersectsFilter (e não um objeto tipo BoundingBoxXYZ
-#_______________________________________________________________________
+    cod_paredes = amb.LookupParameter('COD-REVEST_PAREDES')
+    cod_paredes2 = amb.LookupParameter('COD-REVEST2_PAREDES')
+    cod_piso = amb.LookupParameter('COD-REVEST_PISO')
+    cod_teto = amb.LookupParameter('COD-REVEST_TETO')
+    print('{} - {}-{}'.format(amb_name, amb_level, amb_area))
 
-    cod_paredes = ambiente.LookupParameter('COD-REVEST_PAREDES')
-    cod_paredes2 = ambiente.LookupParameter('COD-REVEST2_PAREDES')
-    cod_piso = ambiente.LookupParameter('COD-REVEST_PISO')
-    cod_teto = ambiente.LookupParameter('COD-REVEST_TETO')
-    print('{} - {}'.format(ambiente_name, ambiente_level))
-
-    ambiente_como_filtro = DB.BoundingBoxIntersectsFilter(ambiente_outline)# Create filter
-    intersecting_elements = DB.FilteredElementCollector(doc).WherePasses(ambiente_como_filtro).ToElements()    # Use filter to retrieve elements
-    # lista_python_collected_elements = ['Ambiente {}: {}'.format(ambiente),list(collected_intersecting_elements)]
+    amb_como_filtro = DB.BoundingBoxIntersectsFilter(amb_outline)# Create filter
+    elementos_intersectantes = DB.FilteredElementCollector(doc).WherePasses(amb_como_filtro).ToElements()    # Use filter to retrieve elements
+    # lista_python_collected_elements = ['Ambiente {}: {}'.format(amb),list(collected_intersecting_elements)]
     categorias_em_PTBR = ['Paredes', 'Pisos', 'Forros']
-    categorias_relevantes = [
-        str(DB.BuiltInCategory.OST_Ceilings),
-        str(DB.BuiltInCategory.OST_Walls),
-        str(DB.BuiltInCategory.OST_Floors)
-    ]
-    cont_elem = 0
+
+    forros_categ = str(DB.BuiltInCategory.OST_Ceilings)
+
+    paredes_categ = str(DB.BuiltInCategory.OST_Walls)
+    pisos_categ = str(DB.BuiltInCategory.OST_Floors)
+
+    categorias_relevantes = [forros_categ, paredes_categ, pisos_categ]
+
     print('ENCONTRADOS os seguintes objetos:')
-
-    for element in intersecting_elements:
-        #inserir aqui regra de q p o elemento contar, deve passar por um 2o filtro, o de se for forro ou piso, "pertencer àquele ambiente. é possível? pq
-        # ta dificl adivinhar atraves dos valores das coordenadas qual seria um valor ideal p deixar de for aos elementos de ambiente adjacentes.
-
+    for elem in intersecting_elements:
+        # SEM O TRY/EXCEPT ELE NAO CONSEGUE ITERAR POR TODOS OS ELEMENTOS:
         try:
-            if any(str(element.Category.BuiltInCategory) == categoria for categoria in categorias_relevantes):
-                print('{}, {}, cód. ID {}'.format(element.Name, element.Category.Name, element.Id))
-                cont_elem += 1
-                element_type_id = element.GetTypeId()
-                element_type = doc.GetElement(element_type_id)
-                estrut_element = DB.HostObjAttributes.GetCompoundStructure(element_type)
-                quant_camadas = estrut_element.LayerCount
-                camadas_estrut = estrut_element.GetLayers()
-                print(element_type.Name)
-                for camada in camadas_estrut:
+            # DEFINIÇÕES:
+            categ = str(elem.Category.BuiltInCategory)
+
+            if any(str(elem.Category.BuiltInCategory) == categoria for categoria in categorias_relevantes):
+                print('{}, {}, cód. ID {}'.format(elem.Name, elem.Category.Name, elem.Id))
+                elem_type_id = elem.GetTypeId()
+                elem_type = doc.GetElement(elem_type_id)
+                elem_estrut = DB.HostObjAttributes.GetCompoundStructure(elem_type)
+                camadas = elem_estrut.GetLayers()
+                quant_camadas = elem_estrut.LayerCount
+                for camada in camadas:
+                    mat_rev_parede = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
+                    print(mat_rev_parede.Name)
                     funcao = camada.Function.ToString()
                     if funcao == 'Finish2' or funcao == 'Membrane':
                         try:
-                            if element.Category.Name == 'Paredes':
+                            if elem.Category.Name == 'Paredes':
                                 # acessar objeto dentre lista Materiais a partir do id do mat. atribuído à camada de revestimento Finish2
-                                material_rev_parede = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
-                                print(material_rev_parede.Name, element.Name)
+                                mat_rev_parede = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
                                 for material in materiais:
                                     if material.Id == camada.MaterialId:
                                         revest_parede = material
@@ -102,9 +87,9 @@ for ambiente in ambientes_em_teste:
                                 t.Start()
                                 cod_paredes.Set(revest_parede.Id)
                                 t.Commit()
-                            elif element.Category.Name == 'Pisos':
-                                material_rev_piso = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
-                                print(material_rev_piso.Name, element.Name)
+                            elif elem.Category.Name == 'Pisos':
+                                mat_rev_piso = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
+                                print(mat_rev_piso.Name, elem.Name)
                                 for material in materiais:
                                     if material.Id == camada.MaterialId:
                                         revest_piso = material
@@ -113,25 +98,28 @@ for ambiente in ambientes_em_teste:
                                 t.Start()
                                 cod_piso.Set(revest_piso.Id)
                                 t.Commit()
-                            elif element.Category.Name == 'Forros':
-                                material_rev_forro = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
-                                print(material_rev_forro.Name, element.Name)
-                                for material in materiais:
-                                    if material.Id == camada.MaterialId:
-                                        revest_forro = material
-                                        print('ok, material associado a FORRO:', revest_forro.Name)
-                                t = DB.Transaction(doc, "aplicar cod revest forro a ambiente")
-                                t.Start()
-                                cod_teto.Set(revest_forro.Id)
-                                t.Commit()
-
+                            elif categ == forros_categ:
+                                forro_area = elem.LookupParameter('Área').AsValueString()
+                                forro_area_num = float((forro_area)[:5])
+                                if amb_area_num * .9 < forro_area_num < amb_area_num * 1.1:
+                                    print('o forro do ambiente {} é: {}, {}, ID nº {}'.format(amb_name,elem.Name,elem.Category.Name,elem.Id))
+                                    mat_rev_forro = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
+                                    print(mat_rev_forro.Name, elem.Name)
+                                    for material in materiais:
+                                        if material.Id == camada.MaterialId:
+                                            revest_forro = material
+                                            print('ok, material associado a FORRO:', revest_forro.Name)
+                                    t = DB.Transaction(doc, "aplicar cod revest forro a ambiente")
+                                    t.Start()
+                                    cod_teto.Set(revest_forro.Id)
+                                    t.Commit()
                         except AttributeError as e:
                             print('erro ao tentar printar nome do objeto:', camada.Id, e)
                     # elif camada.Function.ToString() == 'Membrane' or camada.LayerId == quant_camadas - 1:
-                    # elif element.Category.Name == 'Forros' and camada.LayerId == quant_camadas - 1:
-                    #     print('element.Category.Name')
-                    #     material_rev_forro = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
-                    #     print(material_rev_forro.Name, element.Name)
+                    # elif elem.Category.Name == 'Forros' and camada.LayerId == quant_camadas - 1:
+                    #     print('elem.Category.Name')
+                    #     mat_rev_forro = doc.GetElement(DB.ElementId(int(camada.MaterialId.ToString())))
+                    #     print(mat_rev_forro.Name, elem.Name)
                     #     for material in materiais:
                     #         if material.Id == camada.MaterialId:
                     #             revest_forro = material
@@ -142,26 +130,3 @@ for ambiente in ambientes_em_teste:
                     #     t.Commit()
         except AttributeError:
             pass
-
-
-
-
-
-
-
-
-
-
-
-# SINTAXES QUE FUNCIONARAM:
- # pra retornar nome (string) de tipo de objeto (ex: nome do tipo da parede tal)
-# for wall_instancia in all_walls_list:
-#     titulo = wall_instancia.get_Parameter(DB.BuiltInParameter.ELEM_TYPE_PARAM)
-#     print(titulo.AsValueString())
-
-# for parede_tipo in paredes_tipos:
-#     id = parede_tipo.Id
-#     titulo_tipo = parede_tipo.get_Parameter(DB.BuiltInParameter.ALL_MODEL_TYPE_NAME)
-#     print(titulo_tipo.AsString())
-
-# revestimentos_coletados = [material.Name for material in materiais if material.Id == id_material_camada]
