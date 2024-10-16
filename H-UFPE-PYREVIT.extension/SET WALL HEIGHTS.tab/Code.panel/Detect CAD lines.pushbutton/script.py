d295ba0f8cc02be4a9a0550f21236ea95fa82d65
haudacia <hadassa.codes@gmail.com>
2024-10-17 01:22:36 +0200

    WIP fix: eliminate wall duplicates and eliminate wall instance creation when lines are shorter that a given value

--- H-UFPE-PYREVIT.extension/SET WALL HEIGHTS.tab/Code.panel/Detect CAD lines.pushbutton/script.py
+++ H-UFPE-PYREVIT.extension/SET WALL HEIGHTS.tab/Code.panel/Detect CAD lines.pushbutton/script.py
@@ -11,71 +11,88 @@ uidoc = __revit__.ActiveUIDocument
 doc = uidoc.Document
 
 app = __revit__.Application
-double_to_meter_divisor = 3.28084
+meter_to_double_factor = 3.28083989466
 
-# Coletar todas as linhas do modelo
+# collect all model lines (ImportInstance must be exploded to allow access to the CAD lines)
 model_lines = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Lines).ToElements()
 
-# Coletar o primeiro tipo de parede disponível
-wall_type = DB.FilteredElementCollector(doc).OfClass(DB.WallType).FirstElement()
-if not wall_type:
-    print("Nenhum tipo de parede encontrado.")
+# wall_types_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Walls).WhereElementIsElementType()
+default_wall_type = None
+wall_types_collector = DB.FilteredElementCollector(doc).OfClass(DB.WallType)
 
-# Coletar o primeiro nível disponível
+for wall_type in wall_types_collector:
+    for param in wall_type.Parameters:
+        # if p.Definition.Name == "Nome do tipo":
+        if param.Definition.BuiltInParameter == DB.BuiltInParameter.ALL_MODEL_TYPE_NAME:
+            wall_type_name = param.AsString()
+            if wall_type_name == "ALV_9CM": #to be reviewed(use the least specific nomenclature possible)
+                default_wall_type = wall_type        
+
 level = DB.FilteredElementCollector(doc).OfClass(DB.Level).FirstElement()
+print(level.Id)
 if not level:
-    print("Nenhum nível encontrado.")
+    print("No levels were found")
 
 horizontal_lines = []
 vertical_lines = []
-
+unique_pairs = set()
+default_wall_thickness = 0.492126 # equivalent to 150mm. To be refactored to include more wall thickness values
 
 def find_line_pair(line_list, first_line):
     for line in line_list:
         # print(round(0.49212598425128817, 6))
         # print(round(abs(first_line.GeometryCurve.GetEndPoint(0)[1] - line.GeometryCurve.GetEndPoint(0)[1]), 6))
-        if round(abs(first_line.GeometryCurve.GetEndPoint(0)[1] - line.GeometryCurve.GetEndPoint(0)[1]), 6) == 0.492126:
-            return (first_line, line)
-
-# criar tuplas com funcao acima
+        if round(abs(first_line.GeometryCurve.GetEndPoint(0)[1] - line.GeometryCurve.GetEndPoint(0)[1]), 6) == default_wall_thickness:
+            return frozenset([first_line, line])
 
 for line in model_lines:
     if line.LineStyle.Name == "DPP-PAREDE" and line.GeometryCurve.IsBound:
+        # level = line.SketchPlane #reference level through SketchPlane object's Name property(Name are supposedly equivalent in both objects)
         # print(line.GeometryCurve.GetEndPoint(0)[0])
-        x_startpoint = line.GeometryCurve.GetEndPoint(0)[0]
-        x_endpoint = line.GeometryCurve.GetEndPoint(1)[0]
+        x_startpoint = line.GeometryCurve.GetEndPoint(0).X
+        x_endpoint = line.GeometryCurve.GetEndPoint(1).X
         if x_startpoint - x_endpoint == 0:
             vertical_lines.append(line)
         else:
-            y_startpoint = line.GeometryCurve.GetEndPoint(0)[1]
-            y_endpoint = line.GeometryCurve.GetEndPoint(1)[1]
+            # y_startpoint = line.GeometryCurve.GetEndPoint(0)[1]
+            # y_endpoint = line.GeometryCurve.GetEndPoint(1)[1]
+          
+            y_startpoint = line.GeometryCurve.GetEndPoint(0).Y + (default_wall_thickness/2)
+            y_endpoint = line.GeometryCurve.GetEndPoint(1).Y + (default_wall_thickness/2)
+            # y_endpoint = line.GeometryCurve.GetEndPoint(1).Y
             horizontal_lines.append(line)
+            # print(y_endpoint)
 
-# print(len(horizontal_lines), vertical_lines)
 for horizontal_line in horizontal_lines:
-    tuple = find_line_pair(horizontal_lines, horizontal_line)
-    print(tuple[0].GeometryCurve.GetEndPoint(0), 'tupla--------------------', tuple[1].GeometryCurve.GetEndPoint(0))
-        # Verificar se level e wall_type estão definidos
-    if wall_type and level and tuple is not None:
-        start_point = tuple[0].GeometryCurve.GetEndPoint(0)
-        # print(start_point)
-        end_point = tuple[0].GeometryCurve.GetEndPoint(1)
-        bound_line = DB.Line.CreateBound(start_point, end_point)
+    pair = find_line_pair(horizontal_lines, horizontal_line)
+    # print(pair[0].GeometryCurve.GetEndPoint(0), '--------------------', pair[1].GeometryCurve.GetEndPoint(0))
 
-        t = DB.Transaction(doc, "Create new wall instance from cad lines")
-        # t.Start()
-        # try:
-        #     new_wall = DB.Wall.Create(doc, bound_line, wall_type.Id, level.Id, 10, 0, False, False)
-        #     print("Parede criada entre {} e {}.".format(start_point, end_point))
-        # except Exception as e:
-        #     print("Erro ao criar a parede: {}".format(e))
-        #     pass
-        # t.Commit()
+    if pair is not None and pair not in unique_pairs:
+        unique_pairs.add(pair)
+        print(len(unique_pairs), "devem ser unicos no repetitions")
+        line1, line2 = pair
+        # print(line1.Id, '--', line1.GeometryCurve.GetEndPoint(0), '-------------------', line2.Id, '--', line2.GeometryCurve.GetEndPoint(0))
+
+        if default_wall_type and level:
+              # # ---------Below, the intention is to store a displaced value, as the start_point and end_point 
+            # #, so that he wall is positioned at the center of the two lines that represent its external faces
+            start_point = line1.GeometryCurve.GetEndPoint(0) + DB.XYZ(0, default_wall_thickness/2, 0)
+            # print(start_point)
+            end_point = line1.GeometryCurve.GetEndPoint(1) + DB.XYZ(0, default_wall_thickness/2, 0)
+            bound_line = DB.Line.CreateBound(start_point, end_point)
+
+            t = DB.Transaction(doc, "Create new wall instance from cad lines")
+            t.Start()
+            try:
+                new_wall = DB.Wall.Create(doc, bound_line, default_wall_type.Id, level.Id, 10, 0, False, False)
+                print("Wall created at {} and {}.".format(start_point, end_point))
+            except Exception as e:
+                print("Erro ao criar a parede: {}".format(e))
+                pass
+            t.Commit()
 print('Ok')
 
 # for l in vertical_lines:
-# ???
-
 
 
 
