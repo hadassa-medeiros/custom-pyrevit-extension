34d98558a8fd40d31075fc1e3bba8036200ea5a6
Hadassa <hadassa.codes@gmail.com>
2025-01-14 16:31:35 -0300

    includes diagonal wall lines, WIP refactor and check for CAD drawing inconsistencies

--- CCBI-SPO.extension/H-UFPE-PYREVIT.tab/Code.panel/cad_lines_to_walls.pushbutton/script.py
+++ CCBI-SPO.extension/H-UFPE-PYREVIT.tab/Code.panel/cad_lines_to_walls.pushbutton/script.py
@@ -9,13 +9,13 @@ collect = DB.FilteredElementCollector(doc)
 interface = RevitDocInterface()
 
 # find groups of lines that represent the faces of a same wall and store them
-default_wall_thickness = meter_to_double(0.15)
-# print(default_wall_thickness)
+default_wall_width = meter_to_double(0.15)
+# print(default_wall_width)
 default_walltype_id = find_id_by_element_name(interface.walltypes, "GENERICA_15CM")
 
 cad_wall_lines = interface.filter_lines_by_name(["Parede"])
-tolerance = 1e-5
-
+tolerance = default_wall_width * 0.05 #allows +/- 2% variation to account for minor CAD drawing imprecisions
+# print(tolerance)
 def is_horizontal(model_line):
     return abs(model_line.end_y - model_line.start_y) == 0
 
@@ -28,13 +28,16 @@ def is_diagonal(model_line):
 def offset_equals_wall_width(line_A, line_B):
     def dist_between_lines():
         if is_horizontal(line_B) and is_horizontal(line_A):
-            offset = (abs(line_B.start_y - line_A.start_y))
+            return abs(line_B.start_y - line_A.start_y)
         elif is_vertical(line_B) and is_vertical(line_A):
-            offset = (abs(line_B.start_x - line_A.start_x))
+            return abs(line_B.start_x - line_A.start_x)
+        # elif is_diagonal(line_A) and is_diagonal(line_B):
+            
         else:
-            offset = 0
-        return offset
-    if (dist_between_lines() - default_wall_thickness) < tolerance:
+            return default_wall_width
+    offset = dist_between_lines()
+    # print(abs(offset - default_wall_width))
+    if abs(offset - default_wall_width) < tolerance:
         return True
     else:
         return False
@@ -97,32 +100,39 @@ def share_point_in_perpendicular_axis(line_A, line_B):
 grouped_lines = []
 
 i = 0
-for i in range(len(cad_wall_lines)-1):
-    n = i+1
+for i in range(len(cad_wall_lines)):
+    def lines_have_minimum_length(line_A, line_B):
+        min_length = default_wall_width*1.5
+        def line_length(line):
+            return line.get_Parameter(DB.BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble()
+        # print(line_length(line_A), line_length(line_B))
+        return line_length(line_A) >= min_length and line_length(line_B) >= min_length
+    
     ref_line = ModelLine(cad_wall_lines[i])
     lines_of_same_wall = [ref_line]
+    
+    n = i+1
     while n < len(cad_wall_lines):
         next_line = ModelLine(cad_wall_lines[n])
         # print('comparing now {} to {}:'.format(str(cad_wall_lines[i].Id), str(cad_wall_lines[n].Id)))
-        # print(share_point_in_perpendicular_axis(ref_line, next_line))
-        # print(offset_equals_wall_width(ref_line, next_line))
-        # print(are_parallel(ref_line, next_line))
-        if are_parallel(
-                ref_line, next_line
-            ) and offset_equals_wall_width(
-                ref_line, next_line
-            ) and share_point_in_perpendicular_axis(
-                ref_line, next_line
-            ):
+        # print('have intersecting points?', share_point_in_perpendicular_axis(ref_line, next_line))
+        # print('offset equals width?', offset_equals_wall_width(ref_line, next_line))
+        # print('parallel?', are_parallel(ref_line, next_line))
+        # print(lines_have_minimum_length(cad_wall_lines[i], cad_wall_lines[n]))
+        if (
+            are_parallel(ref_line, next_line)
+            and offset_equals_wall_width(ref_line, next_line)
+            and share_point_in_perpendicular_axis(ref_line, next_line)
+            and lines_have_minimum_length(cad_wall_lines[i], cad_wall_lines[n])
+        ):
             lines_of_same_wall.append(next_line)
-        # print(len(lines_of_same_wall))
         n+=1
+
     if len(lines_of_same_wall) > 1:
         grouped_lines.append(lines_of_same_wall)
     i+=1
 
-print(grouped_lines)
-
+# print(grouped_lines)
 def create_wall(doc, bound_line, default_wall_type_id, level_id, misterious_param_1 = 10, misterious_param_2 = 1, flag_1 = False, flag_2 = False):
     t = DB.Transaction(doc, "Create new wall instance from cad line")
     t.Start()
@@ -145,13 +155,6 @@ def longest(line_list):
         longest = line_list[1]
     return longest
 
-def shortest_or_equal(line_list):
-    if line_list[0].length < lines[1].length:
-        shortest = line_list[0]
-    else:
-        shortest = line_list[1]
-    return shortest
-
 def equal_length(line_list):
     if (line_list[0].length - lines[1].length) < tolerance:
         return line_list[0]
@@ -159,36 +162,36 @@ def equal_length(line_list):
         return line_list[1]
 
 for lines in grouped_lines:
-    ref_line = longest(lines) or equal_length(lines)
-    aux_line = shortest_or_equal(lines)
-    # ref_line = shortest(lines)
-    # aux_line = longest(lines)
-    # # determine line positioning condition relative to the other(s) in the same group
-    print(ref_line.start_x, aux_line.start_x)
-    is_right = is_vertical(ref_line) and ref_line.start_x > aux_line.start_x
-    is_left = is_vertical(ref_line) and ref_line.start_x < aux_line.start_x
-    # print(is_right, is_vertical(ref_line), 'must be opposite of', is_left)
-    is_above = is_horizontal(ref_line) and ref_line.start_y > aux_line.start_y
-    is_below = is_horizontal(ref_line) and ref_line.start_y < aux_line.start_y
+    # Ordena as linhas do grupo pelo menor ponto inicial (start_x, start_y)
+    sorted_lines = sorted(
+        lines,
+        key=lambda line: (line.start_x, line.start_y)
+    )
+    print(sorted_lines)
+    # Seleciona a primeira como linha de referência e a segunda como linha auxiliar
+    ref_line = sorted_lines[0]
+    aux_line = sorted_lines[1]
+    # print(ref_line.start_x, aux_line.start_x)
+    # Vetores direção para linha de referência
+    ref_dir = DB.XYZ(ref_line.end_x - ref_line.start_x, ref_line.end_y - ref_line.start_y, 0)
 
-    if is_right:
-        a = ref_line.start_point - DB.XYZ(default_wall_thickness/2, 0, 0)
-        b = ref_line.end_point - DB.XYZ(default_wall_thickness/2, 0, 0)
-    elif is_left:
-        a = ref_line.start_point + DB.XYZ(default_wall_thickness/2, 0, 0)
-        b = ref_line.end_point + DB.XYZ(default_wall_thickness/2, 0, 0)
-    elif is_above:
-        a = ref_line.start_point - DB.XYZ(0, default_wall_thickness/2, 0)
-        b = ref_line.end_point - DB.XYZ(0, default_wall_thickness/2, 0)
-    elif is_below:
-        a = ref_line.start_point + DB.XYZ(0, default_wall_thickness/2, 0)
-        b = ref_line.end_point + DB.XYZ(0, default_wall_thickness/2, 0)
-    else:
-        a = ref_line.start_point + DB.XYZ(0, 0, 0)
-        b = ref_line.end_point + DB.XYZ(0, 0, 0)
+    # Determina deslocamento para criar a linha entre as duas
+    offset_x = default_wall_width / 2 if abs(ref_dir.X) < abs(ref_dir.Y) else 0
+    offset_y = default_wall_width / 2 if abs(ref_dir.Y) < abs(ref_dir.X) else 0
+
+    # Ajusta deslocamento com base na posição relativa
+    if aux_line.start_x > ref_line.start_x:  # Auxiliar à direita
+        offset_x *= -1
+    if aux_line.start_y > ref_line.start_y:  # Auxiliar acima
+        offset_y *= -1
+
+    # Cria os novos pontos deslocados
+    a = ref_line.start_point - DB.XYZ(offset_x, offset_y, 0)
+    b = ref_line.end_point - DB.XYZ(offset_x, offset_y, 0)
+
+    # Tenta criar a nova parede
     try:
         bound_line = DB.Line.CreateBound(a, b)
         create_wall(doc, bound_line, default_walltype_id, interface.levels[0].Id, 10, 0, False, False)
     except NameError as e:
-        print(e)
-        pass
+        print("Erro ao criar parede: {}".format(e))
