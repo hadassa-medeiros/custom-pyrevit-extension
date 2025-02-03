61d7d20ea50432433b159e9a25392816551c2f5f
Hadassa <hadassa.codes@gmail.com>
2025-02-03 14:52:22 +0100

    WIP generate csv with room finishing areas information

--- CCBI-SPO.extension/CCBI.tab/Revisão.panel/Calcular áreas de revestimento.pushbutton/script.py
+++ CCBI-SPO.extension/CCBI.tab/Revisão.panel/Calcular áreas de revestimento.pushbutton/script.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import clr
+import csv
+import subprocess
 import Autodesk.Revit.DB as DB
 clr.AddReference('RevitAPI')
 
@@ -23,7 +25,6 @@ __title__     = "Areas Revestimentos"
 __author__    = "Hadassa Medeiros"
 
 
-
 # Definir categorias relevantes e criar filtro
 relevant_categories = [
     DB.ElementCategoryFilter(cat) for cat in [
@@ -32,28 +33,6 @@ relevant_categories = [
         DB.BuiltInCategory.OST_Floors
         ]
     ]
-# # is_relevant_category = DB.FilteredElementCollector(doc).WherePasses(relevant_categories)
-# print(type(is_relevant_category))
-
-
-# # Coletar elementos no documento ativo
-# collector = DB.FilteredElementCollector(doc).WherePasses(combined_filter)
-
-# #Mostrar todos os elementos das categorias de interesse
-# for elem in collector:
-#     if elem is not None:
-#         elem_type = doc.GetElement(elem.GetTypeId())
-#         if elem_type is not None:
-#             elem_category = elem.Category
-#             elem_type_name = elem_type.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString()
-
-#             # Verificar condição
-#             if 'REV' in elem_type_name or elem_category.Id.IntegerValue in [cat.value__ for cat in relevant_element_categories]:
-#                 print("Elemento {} - Tipo: {} - Categoria: {}".format(elem.Id, elem_type, elem_type_name))
-
-
-# for room in interface.rooms:
-#     print(get_name(room))
 
 # Correct rooms upper limit to avoid collecting floor/wall elements from above
 levels = interface.levels
@@ -70,24 +49,33 @@ selected_rooms_and_names = forms.SelectFromList.show(room_numbers_and_names, but
 selected_room_numbers = [selected.split(" - ")[0] for selected in selected_rooms_and_names] #refers only to the relement's name, for each element selected.
 selected_room_names = [selected.split(" - ")[1] for selected in selected_rooms_and_names]
 
+output_path = 'C:\Users\Hadassa\Documents\UFPE-SPO-CCBI\Relatorios\Areas_rev_ambientes{}.csv'.format(doc.Title)
+arquivo_csv = open(output_path, 'w')
+escritor = csv.writer(arquivo_csv)
+escritor.writerow(['Numero','Nome', 'Nome revestimento', 'Area revestimento', 'Categoria'])
+
+data_by_room = {
+    'Numero': 00,
+    'Nome': '',
+    'Area': 0,
+    'Revestimentos': {'Pisos': [], 'Paredes': [], 'Forros': []}
+    # 'Pisos': 0,
+    # 'Forro': 0
+}
+
 for selected_room_number in selected_room_numbers:
     selected_room_element = next(room for room in interface.rooms if
                                  get_room_number(room) == selected_room_number)
     room = doc.GetElement(selected_room_element.Id)
-    print('{}------------------------------'.format(room.Number))
 
     # Useful information about project's rooms:
-
-
-
     room_default_height_offset = int(3 * 3.28084)  # Value AsDouble that Equals to 2.74m
     room_upper_offset = room.get_Parameter(DB.BuiltInParameter.ROOM_UPPER_OFFSET)
     room_upper_offset_metric = float(room_upper_offset.AsDouble())/3.280840
     room_level_elev = (room.Level).get_Parameter(DB.BuiltInParameter.LEVEL_ELEV).AsDouble()
     room_upper_level = room.get_Parameter(DB.BuiltInParameter.ROOM_UPPER_LEVEL).AsElementId()
     room_id = room.Id.ToString()
-    room_name = get_name(room)
-
+    room_name = get_name(room) 
     room_bbox = room.get_BoundingBox(doc.ActiveView)
     room_outline = DB.Outline(room_bbox.Min, room_bbox.Max)
     # Establishing rooms as filters to elements
@@ -122,18 +110,58 @@ for selected_room_number in selected_room_numbers:
     is_within_room_space,
     combined_categories_filter
 ])
+    data_by_room['Numero'] = selected_room_number
+    data_by_room['Nome'] = room_name
+    data_by_room['Area'] = room_area
 
+    escritor.writerow([
+        data_by_room['Numero'], data_by_room['Nome'], data_by_room['Area']
+                        ])
+   
     elements_intersected_by_room = DB.FilteredElementCollector(doc).WherePasses(combined_filter).ToElements() # Using filter to retrieve elements
-    list_elements_intersected_by_room = ['room {}: {}'.format(room,list(elements_intersected_by_room))]
-    
     for e in elements_intersected_by_room:
-        print(e.Id)
+        #quero que ele so colete pisos q tenham layer com function == Finish2
+        elem_type = doc.GetElement(e.GetTypeId())
+        elem_layers = elem_type.GetCompoundStructure().GetLayers()
+        elem_category = e.get_Parameter(DB.BuiltInParameter.ELEM_CATEGORY_PARAM).AsValueString()
+        for layer in elem_layers:
+            if any(str(layer.Function) == v for v in ['Finish1', 'Finish2']):
+                elem_area = float(
+                    e.get_Parameter(
+                        DB.BuiltInParameter.HOST_AREA_COMPUTED
+                        ).AsValueString()
+                        .split(' ')[0]
+                        )
+                print(elem_area, 'must equal')
 
+                data_by_room['Categoria'] = elem_category
+
+                data_by_room['Revestimentos']['Paredes'].append({e.Name: elem_area})
+                for key,value in data_by_room['Revestimentos'].items():
+                    # escritor.writerow([key, [v[0] for v in value]])
+                # for key,value in ...data_by_room['Revestimentos']
+                #     escritor.writerow([
+                #         data_by_room['Categoria'],
+
+                #         ])
+                # for item in data_by_room['Revestimentos']['Paredes']:
+                #     escritor.writerow([item.keys])
+                    #gerar csv relatorio
+arquivo_csv.close()
+import os
+
+os.startfile(output_path)
+
+        # print(type(elem_compound_structure))
+        
+        
+        # print([[type(layer.LayerId), str(layer.Function)] for layer in elem_layers])
+        # print(e.get_Parameter(DB.BuiltInParameter.HOST_AREA_COMPUTED).AsValueString())
 
 # end of 1st part of filtering process
-# now, "enter" each intersecting wall/floor/ceiling element
-    # check only for Finish2 layers (ignore the others)
-    # double check to see if layer has a valid finishing material assigned to it
+# now, "enter" the structure of each intersecting wall/floor/ceiling element
+    # check only for Finish2 (or Finish1?) layer functions (ignore the others)
+    # extra: double check to see if layer has a valid finishing material assigned to it
     
     #for each room in the project
     #   for each element that passed both filtering phases
@@ -144,7 +172,6 @@ for selected_room_number in selected_room_numbers:
 
 
     # write the data in a csv file and save it
-
 
 
 
