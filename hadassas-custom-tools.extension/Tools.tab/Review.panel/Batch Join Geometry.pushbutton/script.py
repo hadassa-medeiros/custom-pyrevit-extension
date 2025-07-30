a062b1d6250a7863685805a4581cd9328ee5f0c5
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2025-07-30 18:42:10 +0200

    refactor(merge solids in batch tool): extract logic into reusable functions

--- hadassas-custom-tools.extension/Tools.tab/Review.panel/Batch Join Geometry.pushbutton/script.py
+++ hadassas-custom-tools.extension/Tools.tab/Review.panel/Batch Join Geometry.pushbutton/script.py
@@ -1,89 +1,80 @@
 # -*- coding: utf-8 -*-
 import Autodesk.Revit.DB as DB
-from revit_doc_interface import (RevitDocInterface, get_name)
+from revit_doc_interface import (RevitDocInterface)
 from pyrevit import forms
-
 
 interface = RevitDocInterface()
 
 __title__     = "Batch Join Geometry"
-__author__    = "Hadassa Medeiros"
 doc = __revit__.ActiveUIDocument.Document
 
-# listar categorias cujos elementos sejam solidos (passiveis de ter suas geometrias unidas com outras)
-# filtro?
-
-# def find_out_categories_with_solids():
-#     categories_with_solids = []
-#     for elem in DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElements():
-#         if elem.get_Geometry(DB.Options()) is not None:
-#             print(elem)
-#             # categories_with_solids.append(cat)
-#     print(categories_with_solids)
-
-# find_out_categories_with_solids()
-
-
-# pedir para que o usuario selecione categorias cujos elementos intersectantes devem ser unidos
-category_names_and_elements = {
-    DB.BuiltInCategory.OST_Walls: interface.walls,
-    DB.BuiltInCategory.OST_Floors: interface.floors,
-    DB.BuiltInCategory.OST_StructuralColumns: interface.struct_columns,
-    DB.BuiltInCategory.OST_StructuralFraming: interface.beams,
-    DB.BuiltInCategory.OST_Ceilings: interface.ceilings,
-    DB.BuiltInCategory.OST_Columns: interface.columns
+category_names_and_elems = {
+    "Walls": interface.walls,
+    "Floors": interface.floors,
+    "Ceilings": interface.ceilings,
+    "Structural Columns": interface.struct_columns,
+    "Columns": interface.columns,
+    "Beams": interface.beams
 }
 
-# # passar as categorias para funcao que ira lidar com a transaction
-
-selected_category_A = forms.CommandSwitchWindow.show(
-    [key for key in category_names_and_elements], 
-    message="Selecione um nível para verificar os elementos associados"
+level = forms.CommandSwitchWindow.show(
+    [level for level in interface.levels], 
 )
 
-selected_category_B = forms.CommandSwitchWindow.show(
-    [key for key in category_names_and_elements],
-    message="Selecione uma categoria de elementos para verificar os associados ao piso selecionado"
+category_A = forms.CommandSwitchWindow.show(
+    [key for key in category_names_and_elems]
+    # message="Selecione a segunda categoria para unir os elemos"
 )
 
+category_B = forms.CommandSwitchWindow.show(
+    [key for key in category_names_and_elems if key != category_A]
+    # message="Selecione a segunda categoria para unir os elemos"
+)
+
+def get_elem_geometry(elem):
+    return elem.get_Geometry(DB.Options())
+
+def validate_geometry_as_solid(elem_geometry_unit):
+    return isinstance(elem_geometry_unit, DB.Solid) and elem_geometry_unit.Volume > 0
+        
+def elements_overlap(elem_A_geometry_unit, elem_B_geometry_unit):
+    intersection = DB.BooleanOperationsUtils.ExecuteBooleanOperation(
+        elem_A_geometry_unit, 
+        elem_B_geometry_unit,
+        DB.BooleanOperationsType.Intersect
+    )
+    return intersection and intersection.Volume > 0
+        
 def join_geometry(doc, category_A, category_B):
-    elements_A = category_names_and_elements[category_A]
-    elements_B = category_names_and_elements[category_B]
-
-    t = DB.Transaction(doc, "Join cutting solids")
-    t.Start()
+    elems_A = category_names_and_elems[category_A]
+    elems_B = category_names_and_elems[category_B]
 
     try:
-        for elem_A in elements_A:
-            # Obtém a geometria do elemento
-            elem_A_geometry = elem_A.get_Geometry(DB.Options())
-            # print(elem_A.Id, elem_A.Name)
+        for elem in elems_A:
+            elem_geometry = elem.get_Geometry(DB.Options())
 
-            # Itera sobre todos os objetos geométricos do elemento
-            for elem_A_geometry_object in elem_A_geometry:
-                if isinstance(elem_A_geometry_object, DB.Solid) and elem_A_geometry_object.Volume > 0:
-                    for elem_B in elements_B:
+            for elem_A_geometry_unit in elem_geometry:
+                if validate_geometry_as_solid(elem_A_geometry_unit):
+                    for elem_B in elems_B:
                         elem_B_geometry = elem_B.get_Geometry(DB.Options())
                         
-                        for elem_B_geometry_object in elem_B_geometry:
-                            if isinstance(elem_B_geometry_object, DB.Solid) and elem_B_geometry_object.Volume > 0:
-                                intersection = DB.BooleanOperationsUtils.ExecuteBooleanOperation(
-                                    elem_A_geometry_object, 
-                                    elem_B_geometry_object, 
-                                    DB.BooleanOperationsType.Intersect
-                                    )
-                                if intersection and intersection.Volume > 0:
+                        for elem_B_geometry_unit in elem_B_geometry:
+                            if isinstance(elem_B_geometry_unit, DB.Solid) and elem_B_geometry_unit.Volume > 0:      
+                                if elements_overlap(elem_A_geometry_unit, elem_B_geometry_unit):
+                                    t = DB.Transaction(doc, "Join overlapping geometries")
+                                    t.Start()
+
                                     try:
-                                        DB.JoinGeometryUtils.JoinGeometry(doc, elem_A, elem_B)
-                                        print("Elementos unidos: {} (ID {}) + {} (ID {})".format(elem_A.Name, elem_A.Id, elem_B.Name, elem_B.Id))
+                                        
+                                        DB.JoinGeometryUtils.JoinGeometry(doc, elem, elem_B)
+                                        print("elemos unidos: {} (ID {}) + {} (ID {})".format(elem.Name, elem.Id, elem_B.Name, elem_B.Id))
                                     
                                     except Exception as e:
                                         print("Erro ao unir geometria: {}".format(e))
+                                    finally:
+                                        t.Commit()
     except Exception as e:
         print("Erro geral na transação: {}".format(e))
-    finally:
-        # Comita a transação ao final
-        t.Commit()
 
-# chama a funcao para performar o batch join
-join_geometry(doc, selected_category_A, selected_category_B)
+
+join_geometry(doc, category_A, category_B)
